d6a4bdd7eb099651a0a3fae7b562d49df47e0e6c
LAVITGaetan <67626748+LAVITGaetan@users.noreply.github.com>
2021-01-06 14:57:02 +0400

    Update administration.php

--- administration.php
+++ administration.php
@@ -1,89 +1,109 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>HollaCine</title>
-    <link rel="stylesheet" href="administration.css">
 </head>
 
+
+
+
 <body>
-    <div class="container">
-        <div class="ajouter_film">
-            <h2>Ajouter un film</h2>
-            <form action="" method="post">
-                <input type="text" name="titre" placeholder="Entrez le titre">
-                <input type="text" name="synopsis" placeholder="Entrez le synopsis">
-                <input type="text" name="genre" placeholder="Entrez le genre">
-                <input type="text" name="realisateur" placeholder="Entrez le realisateur">
-                <input type="text" name="acteur" placeholder="Entrez l'acteur">
-                <input type="text" name="miniature" placeholder="Entrez le lien de la miniature">
-                <input type="submit" value="Ajouter un film">
-            </form>
-            <?php
-            if (isset($_POST['titre']) & isset($_POST['synopsis']) & isset($_POST['genre']) & isset($_POST['realisateur']) & isset($_POST['acteur']) & isset($_POST['miniature'])) {
-                $bdd = new PDO('mysql:host=127.0.0.1;dbname=hollacine;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-                $reponse = $bdd->query('SELECT * FROM films');
-
-                $requete = 'INSERT INTO films VALUES(NULL, "' . $_POST['titre'] . '", "' . $_POST['synopsis'] . '", "' . $_POST['genre'] . '", "' . $_POST['realisateur'] . '", "' . $_POST['acteur'] . '", "' . $_POST['miniature'] . '")';
-                $resultat = $bdd->query($requete);
-            }
-            ?>
+    <header>
+        <div id="navbar">
+            <h2 href="#default" id="logo">Holla Ciné</h2>
         </div>
+        <div id="navbar_info">
+            <a href="#SERIE">SERIE</a>
+            <a href="#FILM">FILM</a>
+            <a href="#DESSIN ANIME">DESSIN ANIME</a>
+            <a href="#ENFANTS">ENFANTS</a>
+            <a href="#DOCUMENTAIRE">DOCUMENTAIRE</a>
+            <a href="administration.php">&#x2659;</a>
 
-        <div class="modifier_film">
-            <h2>Modifier un film</h2>
-            <form action="" method="post">
-                <input type="text" name="initial-titre" placeholder="Entrez le titre initial">
-                <input type="text" name="new-titre" placeholder="Choisissez un  nouveau titre">
-                <input type="text" name="new-synopsis" placeholder="Choisissez un  nouveau synopsis">
-                <input type="text" name="new-genre" placeholder="Choisissez un  nouveau genre">
-                <input type="text" name="new-realisateur" placeholder="Choisissez un  nouveau realisateur">
-                <input type="text" name="new-acteur" placeholder="Choisissez un  nouvel acteur">
-                <input type="text" name="new-miniature" placeholder="Choisissez une nouvelle minature">
-                <input type="submit" value="Modifier un film">
-            </form>
-            <?php
-            if (isset($_POST['initial-titre'])) {
-                $bdd = new PDO('mysql:host=127.0.0.1;dbname=hollacine;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-                $reponse = $bdd->query('SELECT * FROM films');
-                
-                $requete = "UPDATE films SET titre='" . $_POST['new-titre'] . "',
-        synopsis='" . $_POST['new-synopsis'] . "', genre='" . $_POST['new-genre'] . "',
-        realisateur='" . $_POST['new-realisateur'] . "', acteur='" . $_POST['new-acteur'] . "',
-        miniature='" . $_POST['new-miniature'] . "'WHERE titre='" . $_POST['initial-titre'] . "'";
-                $resultat = $bdd->query($requete);
-            }
-            ?>
         </div>
-
+    </header>
+    <div class="filtres">
+        <!-- <a><input type="text" placeholder="Search..."></a>-->
+        <form action="" method="post">
+            <input type="text" placeholder="Filtrer par genre" name="filter-genre">
+            <input type="submit" value="Filtrer">
+        </form>
+        <form action="" method="post">
+            <input type="text" Placeholder="Filtrer par realisateur" name="filter-realisateur">
+            <input type="submit" value="Filtrer">
+        </form>
         <?php
-        $bdd = new PDO('mysql:host=127.0.0.1;dbname=hollacine;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-        $reponse = $bdd->query('SELECT * FROM films');
-        // while ($donnees= $reponse->fetch()) {
-        //     echo ' <img src="' . $donnees['miniature'] .'">';
-        // }
-        ?>
+        if (isset($_POST['filter-genre'])) {
+            echo '<h3> Films du genre ' . $_POST['filter-genre'] . '</h3>';
+            echo '<div class="container2">';
 
-        <div class="supprimer_film">
-            <h2>Supprimer un film</h2>
-            <form action="" method="post">
-                <input type="text" name="remove-titre" placeholder="Entrez le titre du film">
-                <input type="submit" value="Supprimer un film">
-            </form>
-            <?php
-            if (isset($_POST['remove-titre'])) {
-                $bdd = new PDO('mysql:host=127.0.0.1;dbname=hollacine;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-                $reponse = $bdd->query('SELECT * FROM films');
-                $requete = 'DELETE from films WHERE titre="' . $_POST['remove-titre'] . '"';
-                $resultat = $bdd->query($requete);
+            $bdd = new PDO('mysql:host=localhost;dbname=id15850098_hollacine;charset=utf8', 'id15850098_root', 'e4HTr8qe974!', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            $reponse = $bdd->query('SELECT * FROM films WHERE genre LIKE "' . '%' . $_POST['filter-genre'] . '%' . '"');
+            while ($donnees = $reponse->fetch()) {
+
+                echo '<div class="film">';
+            echo '<div class="titre">' . $donnees['titre'] . '</div>';
+            echo '<div class="miniature"><img class="img" src="' . $donnees['miniature'] . '"></div>';
+            echo '<div class="description">';
+            echo '<span class="infos">' . $donnees['synopsis'] . '<span> </br>';
+            echo '<span class="infos">Acteurs: ' . $donnees['realisateur'] . '<span> </br>';
+            echo '<span class="infos">Réalisé par ' . $donnees['realisateur'] . '<span> </br>';
+            echo '<span class="infos">Genre: ' . $donnees['genre'] . '<span>';
+            echo '</div>';
+            echo '</div>';
             }
-            ?>
-        </div>
+            echo '</div>';
+        }
+        ?>
+        <?php
+        if (isset($_POST['filter-realisateur'])) {
+            echo '<h3>Films réalisés par ' . $_POST['filter-realisateur'] . '</h3>';
+            echo '<div class="container2">';
 
+            $bdd = new PDO('mysql:host=localhost;dbname=id15850098_hollacine;charset=utf8', 'id15850098_root', 'e4HTr8qe974!', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            $reponse = $bdd->query('SELECT * FROM films WHERE realisateur LIKE "' . '%' . $_POST['filter-realisateur'] . '%' . '"');
+            while ($donnees = $reponse->fetch()) {
+
+                echo '<div class="film">';
+            echo '<div class="titre">' . $donnees['titre'] . '</div>';
+            echo '<div class="miniature"><img class="img" src="' . $donnees['miniature'] . '"></div>';
+            echo '<div class="description">';
+            echo '<span class="infos">' . $donnees['synopsis'] . '<span> </br>';
+            echo '<span class="infos">Acteurs: ' . $donnees['realisateur'] . '<span> </br>';
+            echo '<span class="infos">Réalisé par ' . $donnees['realisateur'] . '<span> </br>';
+            echo '<span class="infos">Genre: ' . $donnees['genre'] . '<span>';
+            echo '</div>';
+            echo '</div>';
+            }
+            echo '</div>';
+        }
+        ?>
     </div>
 
+
+    <div class="container">
+        <?php
+        $bdd = new PDO('mysql:host=localhost;dbname=id15850098_hollacine;charset=utf8', 'id15850098_root', 'e4HTr8qe974!', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        $reponse = $bdd->query('SELECT * FROM films');
+        while ($donnees = $reponse->fetch()) {
+            echo '<div class="film">';
+            echo '<div class="titre">' . $donnees['titre'] . '</div>';
+            echo '<div class="miniature"><img class="img" src="' . $donnees['miniature'] . '"></div>';
+            echo '<div class="description">';
+            echo '<span class="infos">' . $donnees['synopsis'] . '<span> </br>';
+            echo '<span class="infos">Acteurs: ' . $donnees['realisateur'] . '<span> </br>';
+            echo '<span class="infos">Réalisé par ' . $donnees['realisateur'] . '<span> </br>';
+            echo '<span class="infos">Genre: ' . $donnees['genre'] . '<span>';
+            echo '</div>';
+            echo '</div>';
+        }
+
+        ?>
+    </div>
 </body>
 
 </html>
